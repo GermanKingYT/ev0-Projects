dbf1e187e488e432e353fc2e75c552888068e2c2
RaIN <razor13372k@gmail.com>
2016-10-22 20:45:32 +0200

    Update

--- ev0-Skin-Editor/SkinEditor/Form1.Designer.cs
+++ ev0-Skin-Editor/SkinEditor/Form1.Designer.cs
@@ -1811,6 +1811,7 @@
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 53;
             this.label1.Text = "label1";
+            this.label1.Visible = false;
             // 
             // Form1
             // 
