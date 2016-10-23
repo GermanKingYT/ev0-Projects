461072c2f5909756d4fdf99ec0fc785a221523d8
RaIN <razor13372k@gmail.com>
2016-10-24 00:57:47 +0200

    Update

--- SmallSkinTool/ev0SetallFN/Form1.Designer.cs
+++ SmallSkinTool/ev0SetallFN/Form1.Designer.cs
@@ -29,34 +29,12 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.label2 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(123, 58);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Set all Skins to FN";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.button2.Location = new System.Drawing.Point(142, 12);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(123, 58);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Reset all Seed\'s";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // openFileDialog1
             // 
@@ -75,7 +53,7 @@
             // button3
             // 
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.button3.Location = new System.Drawing.Point(142, 12);
+            this.button3.Location = new System.Drawing.Point(141, 12);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(123, 58);
             this.button3.TabIndex = 5;
@@ -93,16 +71,25 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(273, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(277, 82);
+            this.ClientSize = new System.Drawing.Size(274, 82);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
@@ -114,13 +101,11 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- SmallSkinTool/ev0SetallFN/Form1.cs
+++ SmallSkinTool/ev0SetallFN/Form1.cs
@@ -22,25 +22,20 @@ namespace ev0SetallFN
 
         public void Form1_Load(object sender, EventArgs e)
         {
-            if (File.Exists("skins.ini"))
+            //Get Directory
+            var parserDirectory = new FileIniDataParser();
+            IniData dataDirectory = parserDirectory.ReadFile("Config.ini");
+
+            string Directory = dataDirectory["Settings"]["Directory"];
+            label1.Text = Directory;
+
+            if (File.Exists(label1.Text))
             {
-                MessageBox.Show("skins.ini was Found!");
-
-                button1.Visible = true;
-                button1.Enabled = true;
-
-                button2.Visible = true;
-                button2.Enabled = true;
-
-                button3.Visible = false;
-                button3.Enabled = false;
-
-                button4.Visible = false;
-                button4.Enabled = false;
+                label2.Text = label1.Text;
             }
             else
             {
-                MessageBox.Show("skins.ini was NOT Found!\r\nPlease select your CSGO Folder!");
+                MessageBox.Show("Please select your Counter Strike Global Offensive Folder!");
 
                 FolderBrowserDialog browser = new FolderBrowserDialog();
 
@@ -51,18 +46,13 @@ namespace ev0SetallFN
                     tempPath = browser.SelectedPath;
                     if (File.Exists(tempPath + @"\ev0lve\skins.ini"))
                     {
-                        button1.Visible = false;
-                        button1.Enabled = false;
-
-                        button2.Visible = false;
-                        button2.Enabled = false;
-
-                        button3.Visible = true;
-                        button3.Enabled = true;
-
-                        button4.Visible = true;
-                        button4.Enabled = true;
                         label2.Text = tempPath + @"\ev0lve\skins.ini";
+
+                        //Write Directory to Config File
+                        var parserConfig = new FileIniDataParser();
+                        var dataConfig = parserConfig.ReadFile("Config.ini");
+                        dataConfig["Settings"]["Directory"] = tempPath + @"\ev0lve\skins.ini";
+                        parserConfig.WriteFile("Config.ini", dataConfig);
                     }
                     else
                     {
@@ -71,110 +61,6 @@ namespace ev0SetallFN
                     }
                 }
             }
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-                var parserWear = new FileIniDataParser();
-                var Wear = parserWear.ReadFile("skins.ini");
-
-                Wear["1"]["SkinWear"] = "0.000001";
-                Wear["2"]["SkinWear"] = "0.000001";
-                Wear["3"]["SkinWear"] = "0.000001";
-                Wear["4"]["SkinWear"] = "0.000001";
-                Wear["7"]["SkinWear"] = "0.000001";
-                Wear["8"]["SkinWear"] = "0.000001";
-                Wear["9"]["SkinWear"] = "0.000001";
-                Wear["10"]["SkinWear"] = "0.000001";
-                Wear["11"]["SkinWear"] = "0.000001";
-                Wear["13"]["SkinWear"] = "0.000001";
-                Wear["14"]["SkinWear"] = "0.000001";
-                Wear["16"]["SkinWear"] = "0.000001";
-                Wear["17"]["SkinWear"] = "0.000001";
-                Wear["19"]["SkinWear"] = "0.000001";
-                Wear["24"]["SkinWear"] = "0.000001";
-                Wear["25"]["SkinWear"] = "0.000001";
-                Wear["26"]["SkinWear"] = "0.000001";
-                Wear["27"]["SkinWear"] = "0.000001";
-                Wear["28"]["SkinWear"] = "0.000001";
-                Wear["29"]["SkinWear"] = "0.000001";
-                Wear["30"]["SkinWear"] = "0.000001";
-                Wear["32"]["SkinWear"] = "0.000001";
-                Wear["33"]["SkinWear"] = "0.000001";
-                Wear["34"]["SkinWear"] = "0.000001";
-                Wear["35"]["SkinWear"] = "0.000001";
-                Wear["36"]["SkinWear"] = "0.000001";
-                Wear["38"]["SkinWear"] = "0.000001";
-                Wear["39"]["SkinWear"] = "0.000001";
-                Wear["40"]["SkinWear"] = "0.000001";
-                Wear["60"]["SkinWear"] = "0.000001";
-                Wear["61"]["SkinWear"] = "0.000001";
-                Wear["63"]["SkinWear"] = "0.000001";
-                Wear["64"]["SkinWear"] = "0.000001";
-                Wear["500"]["SkinWear"] = "0.000001";
-                Wear["506"]["SkinWear"] = "0.000001";
-                Wear["507"]["SkinWear"] = "0.000001";
-                Wear["508"]["SkinWear"] = "0.000001";
-                Wear["509"]["SkinWear"] = "0.000001";
-                Wear["512"]["SkinWear"] = "0.000001";
-                Wear["514"]["SkinWear"] = "0.000001";
-                Wear["515"]["SkinWear"] = "0.000001";
-                Wear["516"]["SkinWear"] = "0.000001";
-
-                parserWear.WriteFile("skins.ini", Wear);
-                MessageBox.Show("Finished!");
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-                var parserSeed = new FileIniDataParser();
-                var Seed = parserSeed.ReadFile("skins.ini");
-
-                Seed["1"]["SkinSeed"] = "0";
-                Seed["2"]["SkinSeed"] = "0";
-                Seed["3"]["SkinSeed"] = "0";
-                Seed["4"]["SkinSeed"] = "0";
-                Seed["7"]["SkinSeed"] = "0";
-                Seed["8"]["SkinSeed"] = "0";
-                Seed["9"]["SkinSeed"] = "0";
-                Seed["10"]["SkinSeed"] = "0";
-                Seed["11"]["SkinSeed"] = "0";
-                Seed["13"]["SkinSeed"] = "0";
-                Seed["14"]["SkinSeed"] = "0";
-                Seed["16"]["SkinSeed"] = "0";
-                Seed["17"]["SkinSeed"] = "0";
-                Seed["19"]["SkinSeed"] = "0";
-                Seed["24"]["SkinSeed"] = "0";
-                Seed["25"]["SkinSeed"] = "0";
-                Seed["26"]["SkinSeed"] = "0";
-                Seed["27"]["SkinSeed"] = "0";
-                Seed["28"]["SkinSeed"] = "0";
-                Seed["29"]["SkinSeed"] = "0";
-                Seed["30"]["SkinSeed"] = "0";
-                Seed["32"]["SkinSeed"] = "0";
-                Seed["33"]["SkinSeed"] = "0";
-                Seed["34"]["SkinSeed"] = "0";
-                Seed["35"]["SkinSeed"] = "0";
-                Seed["36"]["SkinSeed"] = "0";
-                Seed["38"]["SkinSeed"] = "0";
-                Seed["39"]["SkinSeed"] = "0";
-                Seed["40"]["SkinSeed"] = "0";
-                Seed["60"]["SkinSeed"] = "0";
-                Seed["61"]["SkinSeed"] = "0";
-                Seed["63"]["SkinSeed"] = "0";
-                Seed["64"]["SkinSeed"] = "0";
-                Seed["500"]["SkinSeed"] = "0";
-                Seed["506"]["SkinSeed"] = "0";
-                Seed["507"]["SkinSeed"] = "0";
-                Seed["508"]["SkinSeed"] = "0";
-                Seed["509"]["SkinSeed"] = "0";
-                Seed["512"]["SkinSeed"] = "0";
-                Seed["514"]["SkinSeed"] = "0";
-                Seed["515"]["SkinSeed"] = "0";
-                Seed["516"]["SkinSeed"] = "0";
-
-                parserSeed.WriteFile("skins.ini", Seed);
-                MessageBox.Show("Finished!");
         }
 
         private void button3_Click(object sender, EventArgs e)
